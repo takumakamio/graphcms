95c1d877c55d261da91264fcaeee5fb94c04daac
神尾拓馬 <balo@MacBook-Pro.local>
2021-11-12 03:04:31 +0900

    first commit

---
+++ components/PostWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostWidget() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostWidget
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+
+const posts = [
+  { title: "react testing", excerpt: "learn react testing" },
+  { title: "react pipi", excerpt: "learn react pipi" },
+];
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8 bg-gray-100">
+      <Head>
+        <title>CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
